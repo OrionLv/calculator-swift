d39b3370c6c65f43ec6785d34db4895c4548b5b4
lxb <lxb@lxbdeMacBook-Air.local>
2017-03-22 14:31:40 +0800

    Initial Commit

---
+++ caclucate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  caclucate
+//
+//  Created by lxb on 2017/3/22.
+//  Copyright © 2017年 吕汐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
